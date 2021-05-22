4583249f82c111e0673e49f80d5a306d14d589b9
husseys <husseys@spu.edu>
2021-05-21 20:31:31 -0700

    2

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function handleHomePage(request, response){
+    response.sendFile(__dirname + "/" + "index.html");
+});
+
+app.get('/process_get', function handleProcessGet(request, response){
+    let number = request.query.number;
+    var primeStatus = true;
+    for(var i = 2; i <number/2; i++){
+        if (number % i === 0 ){
+            primeStatus = false;
+        }
+    }
+    let retval = "";
+
+    if (primeStatus) {
+        retval = "Number: " + number + "This number is prime"
+    }
+    else {
+        retval = "Number: " + number + "This number is not prime"
+    }
+
+    response.send(retval)
+
+});
+
+var server = app.listen(3000, function ServerListener() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Using Forms and Express, listening at http://%s:$%s", host, port);
+});
